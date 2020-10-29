4a83700a378edac42f844c15ee3ec6acc1812e45
Oleg100500 <73605575+Oleg100500@users.noreply.github.com>
2020-10-29 19:19:18 +0200

    Add files via upload

---
+++ Code.sh
@@ -0,0 +1,14 @@
+read x
+if [ $x = 1 ]
+then echo Faculty
+elif [ $x = 2 ]
+then echo Faculty of
+elif [ $x = 3 ]
+then echo Faculty of Information
+elif [ $x = 4 ]
+then echo Faculty of Information Technologies
+elif [ $x = 5 ]
+then echo Faculty of Information Technologies and
+elif [ $x = 6 ]
+then echo Faculty of Information Technologies and Design
+fi
